5757a6de1ce053c8c3e9572db79e84da78b96300
Mel Incognito <melxincognito@gmail.com>
2021-09-01 17:08:05 -0700

    create object

---
+++ README.md
@@ -0,0 +1,3 @@
+Welcome to my calculator app! Here you'll be able to do basic math functions such as
+add, subtract, multiply and so forth. 
+
--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 
 
-const add = function(a,b) {
-    return a + b;
-  };
-  
-  const subtract = function(a,b) {
-      return a - b;
-  };
-  
-  const sum = function(array) {
-      return array.reduce((total, current) => total + current, 0);
-  };
-  
-  const multiply = function(array) {
-    return array.map(Number).reduce(function(product, value) { return product * value; })
-  
-  };
-  
-  const power = function(a, b) {
-    return Math.pow(a, b);
-  };
-  
-  const factorial = function(num) {
+function multiplyArray(array) {
+    return array.map(Number).reduce(function(product, value) { return product * value; });
+};
+
+function sum(array) {
+    return array.reduce((total, current) => total + current, 0);
+};
+
+function factorial(num) {
     if (num === 0 || num === 1) {
-      return 1;
-    } else {
-      for (let i = num - 1; i >= 1; i--) {
-        num *= i;
+        return 1;
+      } else {
+        for (let i = num - 1; i >= 1; i--) {
+          num *= i;
+        }
+        return num;
       }
-      return num;
-    }
-  };
+};
+
   
+function operate() {
+
+}
+
+const operators = {
+    add: function(a,b){
+        return a + b;
+    } ,
+    minus: function(a, b){
+        return a - b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    },
+    multiply: function(a, b) {
+        return a * b;
+    },
+    power: function(a, b) {
+        return Math.pow(a, b);
+    },
+}
